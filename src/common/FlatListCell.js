74f1f6991ce201da9c5649414bb208c2e3a624bc
bockepechen <bockepechen@163.com>
2018-06-05 10:25:12 +0800

    JYD_20180605001

--- src/common/FlatListCell.js
+++ src/common/FlatListCell.js
@@ -23,35 +23,37 @@ export default class FlatListCell extends PureComponent {
 
   render() {
     return (
-      <TouchableOpacity style={styles.cellContainer}
-        onPress={this._onPress}>
-        <View style={styles.cellTitle}>
-          <Image style={styles.cellTitleImage}
-            source={require('../../res/images/ic_computer.png')}/>
-          <Text style={{color:this.props.selected?'#636363':'blue'}}>{`嘉季丰  ${this.props.data.item}`}</Text>
-        </View>
-        <View style={styles.cellLineContainer}>
-          <View style={styles.cellLine}/>
-        </View>
-        <View style={styles.cellContentContainer}>
-          <View style={styles.cellContent1}>
-            <View style={styles.cellContent1_1}>
-              <Text style={styles.cellContent1_1_txt1}>8.00%</Text>
-              <Text style={styles.cellContent1_1_txt2}>期待年回报率</Text>
+      <View>
+        <TouchableOpacity style={styles.cellContainer}
+          onPress={this._onPress}>
+          <View style={styles.cellTitle}>
+            <Image style={styles.cellTitleImage}
+              source={require('../../res/images/ic_computer.png')}/>
+            <Text style={{color:this.props.selected?'#636363':'blue'}}>{`嘉季丰  ${this.props.data.item}`}</Text>
+          </View>
+          <View style={styles.cellLineContainer}>
+            <View style={styles.cellLine}/>
+          </View>
+          <View style={styles.cellContentContainer}>
+            <View style={styles.cellContent1}>
+              <View style={styles.cellContent1_1}>
+                <Text style={styles.cellContent1_1_txt1}>8.00%</Text>
+                <Text style={styles.cellContent1_1_txt2}>期待年回报率</Text>
+              </View>
+              <View style={styles.cellContent1_2}>
+              <Text style={styles.cellContent1_2_txt1}>90天</Text>
+              <Text style={styles.cellContent1_2_txt2}>服务期限</Text>
+              </View>
             </View>
-            <View style={styles.cellContent1_2}>
-            <Text style={styles.cellContent1_2_txt1}>90天</Text>
-            <Text style={styles.cellContent1_2_txt2}>服务期限</Text>
+            <View style={styles.cellContent2_1}>
+              <Text style={styles.cellContent2_1_txt1}>剩余：1572432.95元</Text>
+              <TouchableOpacity style={styles.cellContent2_1_btn1}>
+              <Text style={styles.cellContent2_1_btn1_txt}>购买</Text>
+              </TouchableOpacity>
             </View>
           </View>
-          <View style={styles.cellContent2_1}>
-            <Text style={styles.cellContent2_1_txt1}>剩余：1572432.95元</Text>
-            <TouchableOpacity style={styles.cellContent2_1_btn1}>
-            <Text style={styles.cellContent2_1_btn1_txt}>购买</Text>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
     )
   }
 }
@@ -60,14 +62,15 @@ const styles = StyleSheet.create({
   cellContainer: {
     backgroundColor:'white',
     padding:5, 
-    marginTop:10,
+    marginTop:5,
+    marginBottom:5,
     marginLeft:5,
     marginRight:5,
     shadowColor:'gray',
     shadowOffset:{width:0.5, height:0.5},
     shadowOpacity:0.5,
     shadowRadius:2,
-    elevation:6
+    elevation:2
   },
   cellTitle: {
     flexDirection:'row', 
