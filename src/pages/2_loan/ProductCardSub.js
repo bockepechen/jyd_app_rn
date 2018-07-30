6174fac848eb749d74f904fd111530b4c34568da
hellowoody <woodyhello@qq.com>
2018-07-30 18:29:33 +0800

    JYD_20180730002

--- src/pages/2_loan/ProductCardSub.js
+++ src/pages/2_loan/ProductCardSub.js
@@ -16,6 +16,9 @@ export default class ProductCardSub extends Component {
     super(props);
     // console.log(`参数key: ${this.props.key}`);
   }
+  componentWillReceiveProps(nextProps) {
+    this.props = nextProps;
+  }
   render() {
     return (
       <TouchableHighlight
@@ -26,10 +29,10 @@ export default class ProductCardSub extends Component {
           resizeMode={'stretch'} 
           style={{width:GlobalStyles.WINDOW_WIDTH, height:scaleSize(402), flexDirection:'row'}}>
           <View style={{marginTop:isIOS?scaleSize(78):scaleSize(72), marginLeft:scaleSize(114), width:scaleSize(480), height:scaleSize(234), flexDirection:'column'}}>
-            <Text style={{fontSize:scaleSize(42), fontWeight:'bold', color:'#656565'}}>{'嘉季盈'}</Text>
+            <Text style={{fontSize:scaleSize(42), fontWeight:'bold', color:'#656565'}}>{this.props.data.SellName}</Text>
             <View style={{marginTop:isIOS?scaleSize(54):scaleSize(48), height:scaleSize(66), flexDirection:'row', alignItems:'flex-end'}}>
               <View style={{height:scaleSize(54)}}>
-                <Text style={{fontSize:scaleSize(54), fontWeight:'200', color:'#998675'}}>{6.24}</Text>
+                <Text style={{fontSize:scaleSize(54), fontWeight:'200', color:'#998675'}}>{this.props.data.ExpectedYearYield}</Text>
               </View>
               <View style={{height:scaleSize(32)}}>
                 <Text style={{fontSize:scaleSize(32), fontWeight:'200', color:'#998675'}}>{'%'}</Text>
@@ -38,7 +41,7 @@ export default class ProductCardSub extends Component {
                 <Text style={{fontSize:scaleSize(54), fontWeight:'200', color:'#998675'}}>{'+'}</Text>
               </View>
               <View style={{height:scaleSize(66)}}>
-                <Text style={{fontSize:scaleSize(66), fontWeight:'200', color:'#ff3a49'}}>{3.76}</Text>
+                <Text style={{fontSize:scaleSize(66), fontWeight:'200', color:'#ff3a49'}}>{this.props.data.ExpectedYield}</Text>
               </View>
               <View style={{height:scaleSize(32)}}>
                 <Text style={{fontSize:scaleSize(32), fontWeight:'200', color:'#ff3a49'}}>{'%'}</Text>
@@ -54,10 +57,10 @@ export default class ProductCardSub extends Component {
           </View>
           <View style={{backgroundColor:'#f2f2f2', width:GlobalStyles.PIXEL, marginTop:scaleSize(69), marginBottom:scaleSize(69), marginLeft:scaleSize(27)}}/>
           <View style={{marginTop:scaleSize(78), marginLeft:0, width:scaleSize(522), height:scaleSize(261), flexDirection:'column', alignItems:'center'}}>
-            <Text style={{fontSize:scaleSize(48), color:'#998675'}}>{'服务期限 90天'}</Text>
+            <Text style={{fontSize:scaleSize(48), color:'#998675'}}>服务期限 {' '+this.props.data.ExpiresDays}天</Text>
             {
-              this.props.isRestMoney?
-                (<Text style={{marginTop:scaleSize(18), fontSize:scaleSize(36), color:'#989898'}}>{'剩余 100000元'}</Text>)
+              this.props.data.SurplusMoney?
+                (<Text style={{marginTop:scaleSize(18), fontSize:scaleSize(36), color:'#989898'}}>剩余{' '+this.props.data.SurplusMoney}元</Text>)
                 :null
             }
             <TouchableHighlight
