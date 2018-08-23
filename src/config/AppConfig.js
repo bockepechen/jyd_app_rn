c74634ac58aafaa8243031f8351e1dfa7a8f64ef
hellowoody <woodyhello@qq.com>
2018-08-23 21:07:02 +0800

    JYD_20180823002

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,7 +1,7 @@
 const isProEnv = false;
 export var AppConfig = {
-  // REQUEST_HOST: isProEnv?'https://authoritymserv.jiayecaifu.com:8023':'https://jydrnserv.jiayidai.com:8282/JYD_RN_Serv',
-  REQUEST_HOST:'http://10.2.0.155:8199/JYD_RN_Serv',
+  REQUEST_HOST: isProEnv?'https://authoritymserv.jiayecaifu.com:8023':'https://jydrnserv.jiayidai.com:8282/JYD_RN_Serv',
+  // REQUEST_HOST:'http://10.2.0.155:8199/JYD_RN_Serv',
   isProEnv: isProEnv,
   REQUEST_TIMEOUT:50000,
   AUTHCODE_CD:10,
