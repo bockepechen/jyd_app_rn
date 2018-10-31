c81e56b478dc519d62c3c80937360c3f0cdee86a
hellowoody <woodyhello@qq.com>
2018-10-31 11:00:13 +0800

    JYD_20181031001

--- src/pages/4_my/ProductCardCjz.js
+++ src/pages/4_my/ProductCardCjz.js
@@ -71,14 +71,9 @@ export default class ProductCardCjz extends Component {
     })
   }
 
-  getcjjl(contractexpectamount,improverate,expiresdays){
-    let res = parseFloat(contractexpectamount)*parseFloat(improverate)*parseFloat(expiresdays) / 365
+  getcjjl(amount,improverate,expiresdays){
+    let res = parseFloat(amount)*parseFloat(improverate)*parseFloat(expiresdays) / 365
     return Utils.toFixed2(res)
-  }
-
-  getYjlx(expectedyearyield,contractexpectamount,expiresdays){
-    let res = parseFloat(expectedyearyield) * parseFloat(contractexpectamount) * parseFloat(expiresdays) / 365
-    return res.toFixed(2)
   }
 
   getqjnhbl(expectedyearyield,improveyearrate){
@@ -183,7 +178,7 @@ export default class ProductCardCjz extends Component {
                             <Text style={{fontSize:scaleSize(36),color:'#989898',marginTop:scaleSize(15)}}>{'预期利息(元)'}</Text>
                         </View>
                         <View style={{marginLeft:scaleSize(155)}}>
-                            <Text style={{fontSize:isIOS ? scaleSize(48) : scaleSize(46),color:'#ff3a49',height:isIOS ? scaleSize(49) : scaleSize(53)}}>{this.getcjjl(this.paraData.contractexpectamount,this.paraData.improveyearrate,this.paraData.expiresdays)}</Text>
+                            <Text style={{fontSize:isIOS ? scaleSize(48) : scaleSize(46),color:'#ff3a49',height:isIOS ? scaleSize(49) : scaleSize(53)}}>{this.getcjjl(this.paraData.contractamount,this.paraData.improveyearrate,this.paraData.expiresdays)}</Text>
                             <Text style={{fontSize:scaleSize(36),color:'#989898',marginTop:scaleSize(15)}}>{'出借奖励(元)'}</Text>
                         </View>
                     </View>
